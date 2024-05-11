4caed6a48007c321f140debc9bdaf01c58526563
vuong <doducvuong2003@gmail.com>
2024-05-11 07:28:13 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FBC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
