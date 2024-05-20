630eb86a2b9c3ba267b4ae75187e0a9cc2b1274a
NGUYEN DAC HOANG DAT <datndhhe170720@fpt.edu.vn>
2024-05-20 21:46:05 +0700

    update class user

---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace FBC.Models;
+
+public partial class User : IdentityUser
+{
+    public string? Address { get; set; }
+
+    public DateOnly? Dob { get; set; }
+
+    public bool? Gender { get; set; }
+
+
+    public virtual ICollection<BookOrder> BookOrders { get; set; } = new List<BookOrder>();
+
+    public virtual ICollection<CartOrder> CartOrders { get; set; } = new List<CartOrder>();
+
+    //public virtual Account? EmailNavigation { get; set; }
+
+    public virtual ICollection<ExchangeRequest> ExchangeRequests { get; set; } = new List<ExchangeRequest>();
+
+    public virtual ICollection<Wallet> Wallets { get; set; } = new List<Wallet>();
+}
