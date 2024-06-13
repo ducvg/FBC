716749fadcca6f7a691b491a41b36841fe82c26c
William Kieu <williamkieu-devops@outlook.com>
2024-06-13 23:37:28 +0700

    change pipeline

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 kill -9 $(lsof -t -i :5062)
-dotnet build
-dotnet publish -c Release -o ./publish
+# rm -rf publish
+# dotnet build
+# dotnet publish -c Release -o ./publish
 # dotnet publish/FBC.dll
 dotnet run
